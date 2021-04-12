e474386314c1133521d8e92192e5f87f2021ec51
kim-yujin497 <80802566+kim-yujin497@users.noreply.github.com>
2021-04-12 20:19:41 +0900

    Add files via upload

---
+++ hk4.php
@@ -0,0 +1,32 @@
+<?php
+
+$triangle_width = $_GET[triangle_width] 
+$triangle_height = $_GET[triangle_height]
+$triangle = $triangle_width*$triangle_height;
+echo $triangle;
+
+$oblong_width = $_GET[oblong_width]
+$oblong_height = $_GET[oblong_height]
+$oblong = $oblong_width*$oblong_height;
+echo $oblong;
+
+$circle_radius = $_GET[circle_radius]
+$PI =3.14;
+$circle = $circle_radius*$PI;
+echo $circle;
+
+$Cuboid_width = $_GET[Cuboid_width]
+$Cuboid_length = $_GET[Cuboid_length]
+$Cuboid_height = $_GET[Cuboid_height]
+$Cuboid =$Cuboid_width*$Cuboid_length*$Cuboid_height;
+
+$cylinder_radius = $_GET[cylinder_radius]
+$cylinder_height = $_GET[cylinder_height]
+$PI =3.14;
+$cylinder =$cylinder_radius*$cylinder_radius*$cylinder_height*$PI;
+
+$phrase_radius = $_GET[phrase_radius]
+$PI =3.14;
+$phrase =4.0/38$PI*$phrase_radius*$phrase_radius*$phrase_radius
+
+?>
